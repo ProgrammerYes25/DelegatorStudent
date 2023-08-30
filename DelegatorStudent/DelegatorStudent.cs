157e7b5140929db734112a4b3eccba3a65be6f39
yes25 <programmer.yes25@gmail.com>
2023-08-30 12:06:59 +0900

    델리게이터와 람다 활용 완료!

--- DelegatorStudent/DelegatorStudent.cs
+++ DelegatorStudent/DelegatorStudent.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace DelegatorStudent
 {
-    internal class Program
+    internal class DelegatorStudent
     {
+        
+
         static void Main(string[] args)
         {
+            Students students = new Students();
+            students.Add(new Student("정가현", 4.5));
+            students.Add(new Student("임수연", 4.6));
+
+            students.Print();
+            students.Print((student) =>
+            {
+                Console.WriteLine();
+                Console.WriteLine("이름 : " + student.Name);
+                Console.WriteLine("학점 : " + student.Score);
+            });
         }
     }
 }
