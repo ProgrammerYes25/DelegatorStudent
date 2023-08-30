575afa57a5fccd05ac66ed92b87002056ffd83f3
yes25 <programmer.yes25@gmail.com>
2023-08-30 12:02:12 +0900

    Students 완성

---
+++ DelegatorStudent/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatorStudent
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public double Score { get; set; }
+        public Student(string Name, double Scoer) { 
+            this.Name = Name;
+            this.Score = Scoer;
+        }
+        public override string ToString()
+        {
+            return this.Name+" :"+this.Score;
+        }
+    }
+}
---
+++ DelegatorStudent/Students.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatorStudent
+{
+    internal class Students
+    {
+        private List<Student> listOfStudent = new List<Student>();
+
+        public delegate void printProcess(Student student);
+
+        public void Add(Student student)
+        {
+            listOfStudent.Add(student);
+        }
+        public void Print()
+        {
+            Print((student) =>
+            {
+                Console.WriteLine(student);
+            });
+
+        }
+
+        public void Print(printProcess process)
+        {
+            foreach (var item in listOfStudent)
+            {
+                process(item);
+            }
+        }
+    }
+}
